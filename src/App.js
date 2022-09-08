d0a66fa37cabc177ca6d49721d942dec07d63c7a
YogaSetiawanXsaintz <yogas9079@gmail.com>
2022-09-08 13:50:12 +0700

    commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "./style/LandingPage.css";
+import "./style/intro.css";
+import "./style/Heading.css";
+import "./style/Header.css";
+import "./style/Biografi.css";
+import "./style/Outro.css";
+import "./style/Contact.css";
+import LandingPage from "./components/LandingPage";
+import Intro from "./components/Intro";
+import Heading from "./components/Heading";
+import Header from "./components/Header";
+import Biografi from "./components/Biografi";
+import Outro from "./components/Outro";
+import Contact from "./components/Contact";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <LandingPage />
+      <Intro />
+      <Heading />
+      <Header />
+      <Biografi />
+      <Outro />
+      {/* <MouseParticles g={0} num={6} color={["#ff0000", "white"]} /> */}
+      <Contact />
     </div>
   );
 }
